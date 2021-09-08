9c80f3bcc650ce6d2fa8f1148f14ac83bfea6281
abhishekgaikwad10 <abhishekgaikwad326@gmail.com>
2021-09-08 15:45:16 +0530

    player rolls the die

--- src/com/abhi/empwage/Main.java
+++ src/com/abhi/empwage/Main.java
@@ -3,14 +3,13 @@ package com.abhi.empwage;
 public class Main {
 
     public static void main(String[] args) {
-        //Taking position variable for starting position and assign it zero
-        int position = 0;
-
-        System.out.println("WELCOME TO SNAKE AND LADDER PROGRAMME");
-        System.out.println("You are at Starting Position::" + position);
-
+        
+        //taking dice number by using random
+        int diceCheck = (int) Math.floor(Math.random() * 10) % 6+1;
+        System.out.println("Dice Value::"+ diceCheck);
     }
 
 }
+
     
 
