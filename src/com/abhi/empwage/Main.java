d3a54fdf00949b9978d3701fd927ab78f7ac8c19
abhishekgaikwad10 <abhishekgaikwad326@gmail.com>
2021-09-08 16:01:59 +0530

    no play youare in same position

--- src/com/abhi/empwage/Main.java
+++ src/com/abhi/empwage/Main.java
@@ -3,13 +3,36 @@ package com.abhi.empwage;
 public class Main {
 
     public static void main(String[] args) {
-        
-        //taking dice number by using random
-        int diceCheck = (int) Math.floor(Math.random() * 10) % 6+1;
-        System.out.println("Dice Value::"+ diceCheck);
-    }
 
-}
+                //Taking position variable for starting position and assign it zero
+                int position = 0;
+                System.out.println("position is " + position);
 
-    
+                //Taking die variable for dice number by using random
+                int die = (int) (Math.random() * 10) % 6 + 1;
+                System.out.println("dice value is " + die);
 
+                //Taking value variable by using random to compare with cases
+                int value = (int) (Math.random() * 10) % 3 + 1;
+                System.out.println("case number is " + value);
+
+                //computation
+                switch (value) {
+                    case 1:
+                        System.out.println("No Play You are in the same position.");
+                        break;
+
+                    case 2:
+                        position = (position + die);
+                        System.out.println("your position after ladder is ::" + position);
+                        break;
+
+                    case 3:
+                        position = (position - die);
+                        System.out.println("Your Position after snake is::" + position);
+                        break;
+                }
+            }
+
+
+        }
